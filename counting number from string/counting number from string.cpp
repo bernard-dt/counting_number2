d78df02e2b361cb3601fae0239b58f977c8d0561
Bernardha <139964988+Bernardha@users.noreply.github.com>
2024-10-04 17:04:56 +0900

    first commit

---
+++ counting number from string/counting number from string.cpp
@@ -0,0 +1,19 @@
+﻿
+#include <iostream>
+#include <string>
+
+int main()
+{
+    const char ch[] = "dfk34klgjljlk43klgjklje4lkrtjkl4";
+    int cnt;
+
+    cnt = 0; 
+    for (char c : ch)
+    {
+        if ((c >= '0') && (c <= '9'))
+            cnt++;
+    }
+    std::cout << "count is " << cnt << std::endl;
+    return 0;
+}
+
